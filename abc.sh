091c1b50c68ee5fa418381a46f840ce283acf15a
ishmeet177 <62622851+ishmeet177@users.noreply.github.com>
2022-11-04 20:20:57 +0530

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -2,7 +2,8 @@
  
 while true; do
 sleep 1
-date >> /tmp/tcpdump/"$NODE_NAME".txt 2>&1;nc -zv -w 1 10.177.103.37 6432 -n >> /tmp/tcpdump/"$NODE_NAME".txt 2>&1
+date >> /tmp/tcpdump/"$NODE_NAME".txt 2>&1;nc -zv -w 1 10.177.103.37 6432 >> /tmp/tcpdump/"$NODE_NAME".txt 2>&1
+test
 done
 Footer
 © 2022 GitHub, Inc.
